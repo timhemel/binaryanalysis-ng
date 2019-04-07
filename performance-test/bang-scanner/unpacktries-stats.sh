9f2ed59f002248ee0351cec42eebe2f69b901a79
Tim Hemel <tim@securesoftware.nl>
2019-04-07 11:36:15 +0200

    Fix in script.

--- performance-test/bang-scanner/unpacktries-stats.sh
+++ performance-test/bang-scanner/unpacktries-stats.sh
@@ -2,5 +2,5 @@
 
 d=`dirname "$0"`
 
-( echo unpacker,result,time ; (grep try_ | sed 's/try_.*\[\(unpack.*\)\]: \(.*\) = \(.*\)/\1,\2,\3/')) | python3 "$d"/unpacktries-stats.py 
+( echo unpacker,result,time ; (grep '^try_' | sed 's/try_.*\[\(unpack.*\)\]: \(.*\) = \(.*\)/\1,\2,\3/')) | python3 "$d"/unpacktries-stats.py 
 
